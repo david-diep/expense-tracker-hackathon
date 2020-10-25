14cbee895259a0645f1dae1c61da9447397ad3f0
David Diep <ddkotan@gmail.com>
2020-10-25 00:14:54 -0700

    Updating about page
    touching it up

--- pages/about.js
+++ pages/about.js
@@ -10,41 +10,51 @@ import LocalAtmIcon from '@material-ui/icons/LocalAtm';
 export default function About() {
   return (
     <div style={{ background: '#ADD8E6', minHeight:"100vh" }}>
-    <Container >
-        <Box py={4} style={{
-          background:  '#f5f5f5', minHeight:"90vh"}}>
-          <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px" ,display:'flex', justifyContent:'space-between'}}>
-            <div>
-              <Typography variant="h4"  gutterBottom   >
-                <span style={{padding: '20px 10px 5px 10px', display:'flex', alignItems:'center' }}>Budget Buddy <LocalAtmIcon style={{fontSize:'3rem', marginLeft:'10px'}} /></span>
+      <Container style={{
+        background: '#f5f5f5', padding:'0px'}}>
+        <Box>
+          <Container style={{ background: '#ADD8E6', display: 'flex', justifyContent: 'center',  }}>
+            <div style={{ background: '#2E3B55', color:'#f5f5f5', padding: '20px 100px 20px 20px', marginRight:'20px', }}>
+              <Typography variant="h4" gutterBottom   >
+                <span style={{ padding: '20px 10px 5px 10px', display: 'flex', alignItems: 'center' }}>Budget Buddy <LocalAtmIcon style={{ fontSize: '3rem', marginLeft: '10px' }} /></span>
               </Typography>
-               <h3 style={{marginLeft:"20px",paddingBottom:"10px"}} >Your one-stop expense tracker!</h3>
+              <h3 style={{ marginLeft: "20px", paddingBottom: "10px" }} >Your one-stop expense tracker!
+                <br></br>
+                By <a style={{ color:'#f5f5f5'}} href='https://david-diep.com/' target=''>David Diep</a>
+                <br></br>
+                <a style={{ color: '#f5f5f5' }} href='https://github.com/david-diep/expense-tracker-hackathon'>Click here to checkout the code!</a>
+              </h3>
+
               <Link href="/">
-                <Button variant="contained" style={{ marginLeft: "20px", }}>Check it out!</Button>
+                <Button variant="contained" style={{ marginLeft: "20px", }}>Check the app out!</Button>
               </Link>
             </div>
+          </Container>
+          <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px", display: 'flex', justifyContent:'center'}}>
+            <h3>Work with a seemlessly expandable navigation bar!</h3>
             <img src="/sidebar.gif" style={{ height: '350px', padding: '10px' }}></img>
+
           </div>
 
-          <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px", display: 'flex', }}>
+          <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px", display: 'flex',  justifyContent:'center'}}>
             <img src="/newaccount.gif" style={{ height: '350px', padding: '10px' }}></img>
             <div>
-              <h3>Add New Accounts Seemlessly.</h3>
-              <h3>Rename them too!</h3>
+              <h3>Adding new accounts is easy.</h3>
+              <h3>So is renaming them too!</h3>
             </div>
           </div>
 
-          <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px", display: 'flex', flexDirection:'row-reverse' }}>
+        <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px", display: 'flex', flexDirection: 'row-reverse',  justifyContent:'center' }}>
             <img src="/addexpense.gif" style={{ height: '350px', padding: '10px' }} >
               </img>
             <div>
 
               <h3>Create New Expenses with Elegance.</h3>
-              <h3>Full Editing Power!</h3>
+              <h3>Edit with a click of a button!</h3>
             </div>
           </div>
 
-          <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px", display: 'flex',}}>
+        <div style={{ background: '#2E3B55', color: '#f5f5f5', paddingBottom: "5px", display: 'flex', justifyContent: 'center'}}>
             <img src="/organize.gif" style={{ height: '350px', padding: '10px' }}></img>
             <div>
               <h3>Organize your expenses by category!</h3>
